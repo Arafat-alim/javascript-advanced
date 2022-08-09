e425fb417323be9ecf23ea0bf6da4aa270917c7b
arafat-alim <arafat97alim@gmail.com>
2022-08-09 22:45:47 +0530

    new keyword concept is added.

--- constructors-prototypes/functionInObj.js
+++ constructors-prototypes/functionInObj.js
@@ -1,3 +1,4 @@
+/*
 function createStudent(name, rollNo, marks) {
   let student = {}; // new Object()
   student.name = name;
@@ -8,5 +9,20 @@ function createStudent(name, rollNo, marks) {
 
 let student1 = createStudent("abcs", 12, 45); //Object { name: "abcs", rollNo: 12, marks: 45 }
 let student2 = createStudent("pqrs", 12, 45); //Object { name: "pqrs", rollNo: 12, marks: 45 }
+console.log(student1);
+console.log(student2);
+ */
+
+function createStudent(name, marks, rollNo) {
+  //let student = {}; // new Object()
+  this.name = name;
+  this.marks = marks;
+  this.rollNo = rollNo;
+  //return student;
+}
+
+// here new keyword intialise the object and at the end return an object for us
+let student1 = new createStudent("abcd", 1, 54);
+let student2 = new createStudent("poiu", 2, 72);
 console.log(student1);
 console.log(student2);
