5100f9ed4667dd5adb4f8c18fe19ceb8f761759f
arafat-alim <arafat97alim@gmail.com>
2022-08-31 00:38:41 +0530

    debouncing concept is added

--- Deboucing/script.js
+++ Deboucing/script.js
@@ -1,1 +1,20 @@
-alert("Hello");
+let counter = 0;
+
+function getData() {
+  //Api calls
+  console.log("Fetching Data", counter++);
+}
+
+const debounce = function (fn, d) {
+  let timer;
+  return function () {
+    let context = this;
+    let args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, d);
+  };
+};
+
+let betterFunction = debounce(getData, 300);
