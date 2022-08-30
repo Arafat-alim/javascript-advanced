74a9eedd6b1faff4dc3381281d0aeabe0e1a2a10
arafat-alim <arafat97alim@gmail.com>
2022-08-30 23:48:27 +0530

    Delegation events.

--- DOM/Delegation/example-one.js
+++ DOM/Delegation/example-one.js
@@ -1,4 +1,6 @@
 document.querySelector("#categories").addEventListener("click", (e) => {
-  console.log(e.target.id);
-  window.location.href = "/" + e.target.id;
+  //   console.log(e.target.id);
+  if (e.target.tagName === "LI") {
+    window.location.href = "/" + e.target.id;
+  }
 });
---
+++ DOM/Delegation/example-two.js
@@ -0,0 +1,8 @@
+//! Realtime
+
+document.querySelector("#form").addEventListener("keyup", (e) => {
+  //   console.log(e.target.dataset);
+  if (e.target.dataset.uppercase !== undefined) {
+    e.target.value = e.target.value.toUpperCase();
+  }
+});
