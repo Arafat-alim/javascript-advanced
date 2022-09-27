e7d0d4d5439f2be1220d298f981b4bce3c64e7c5
arafat-alim <arafat97alim@gmail.com>
2022-09-27 15:50:17 +0530

    block scope

---
+++ block-scope/program-one.js
@@ -0,0 +1,30 @@
+//! Global scope
+const name = "Arafat";
+
+function newFunc() {
+  console.log(name);
+}
+newFunc();
+
+function anotherFun() {
+  var go = "Goku";
+  //   console.log(goh); //cant access to this -- not define error
+  function moreFun() {
+    var goh = "gohan";
+    console.log(go);
+  }
+  moreFun();
+}
+
+console.log(anotherFun());
+
+//! block scope
+if (true) {
+  var name1 = "Arafat";
+  let name2 = "Aman";
+  const name3 = "Alim";
+}
+
+console.log(name1); // arafat
+console.log(name2); // not defined
+console.log(name3); //not defined
