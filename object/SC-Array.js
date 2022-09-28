ad904e9f6cf03ab318ba71cab5e8aaca1b68a63d
arafat-alim <arafat97alim@gmail.com>
2022-09-28 22:44:30 +0530

    added comparison on the array program

--- object/SC-Array.js
+++ object/SC-Array.js
@@ -5,10 +5,11 @@ const newNum = [...numbers];
 
 // ! Adding an elmeent to the numbers
 numbers.push(5);
-console.log(numbers);
-console.log(copiedNum);
-console.log(newNum);
+console.log(numbers); // [ 1, 2, 3, 4, 5 ]
+console.log(copiedNum); // [ 1, 2, 3, 4, 5 ]
+console.log(newNum); // [ 1, 2, 3, 4 ]
 
+console.log(numbers === newNum); //false
 //! 2nd method - slice method -- Array
 const num2 = [32, 54, 67, 98];
 const copyNum2 = num2;
@@ -16,6 +17,8 @@ const newNum2 = num2.slice();
 
 // adding an elmeent in the num2
 num2.push(1000000);
+
+console.log(num2 === newNum2); //false
 
 console.log(num2); //[ 32, 54, 67, 98, 1000000 ]
 console.log(copyNum2); //[ 32, 54, 67, 98, 1000000 ]
