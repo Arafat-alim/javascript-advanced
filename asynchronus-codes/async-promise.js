79cc3d351d6b9ee8c56f19909524c2cb375e0719
arafat-alim <arafat97alim@gmail.com>
2022-09-29 20:20:08 +0530

    asyncrhonous code fix with  promise

---
+++ asynchronus-codes/async-promise.js
@@ -0,0 +1,33 @@
+// ! Wrtiting the dependent code of Promise
+
+const fetchUser = (username) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ username });
+    }, 3000);
+  });
+};
+
+const fetchPhotos = (username) => {
+  return new Promise((reolve, reject) => {
+    setTimeout(() => {
+      console.log(`We have the ${username}`);
+      reolve(["Photo 1 ", "Photo 2"]);
+    }, 2000);
+  });
+};
+
+const fetchDetails = (photo) => {
+  return new Promise(() => {
+    setTimeout(() => {
+      console.log(`Now we have the ${photo}`);
+      resolve("Details...");
+    }, 3121);
+  });
+};
+
+// ! Calling the function
+fetchUser("Arafat")
+  .then((user) => fetchPhotos(user))
+  .then((photos) => fetchDetails(photos[0]))
+  .then((details) => console.log(`Your Photo details are - ${details}`));
