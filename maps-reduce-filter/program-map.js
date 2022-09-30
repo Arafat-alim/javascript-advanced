e579782152cf9183c9b19a6ab7635a2e423fc6f0
arafat-alim <arafat97alim@gmail.com>
2022-09-30 19:18:24 +0530

    map function added

---
+++ maps-reduce-filter/program-map.js
@@ -0,0 +1,6 @@
+//! it return a new array, it is basically used to compute an array, peform some operation on it
+let nums = [1, 2, 3, 4];
+// ! Let add each element by one
+let addOne = nums.map((value) => value + 1);
+
+console.log(addOne); //[ 2, 3, 4, 5 ]
