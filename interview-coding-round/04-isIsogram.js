aa0fc144be6f6f793b0d77037e6a1aa6ac736a75
arafat-alim <arafat97alim@gmail.com>
2022-10-02 01:28:13 +0530

    isoIsogram repetition string remove .

---
+++ interview-coding-round/04-isIsogram.js
@@ -0,0 +1,14 @@
+//! If i found any repeition of letters then return false otherwise return true
+
+const str1 = "Ettyide";
+const str2 = "goku";
+
+function isIsogram(string) {
+  const lowerStr = string.toLowerCase();
+  const result = lowerStr
+    .split("")
+    .every((letter, index) => lowerStr.indexOf(letter) === index);
+  return result;
+}
+console.log(isIsogram(str1)); //false
+console.log(isIsogram(str2)); //true
