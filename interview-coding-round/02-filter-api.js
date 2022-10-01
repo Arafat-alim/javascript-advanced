b57f69ea0bf4a43c6371b7461f25849ebb2268c8
arafat-alim <arafat97alim@gmail.com>
2022-10-01 20:47:51 +0530

    filter program adding try catch block for error handling

--- interview-coding-round/02-filter-api.js
+++ interview-coding-round/02-filter-api.js
@@ -1,13 +1,17 @@
 //! Filter out the data - email and names whose starts with "k"
 
 async function getUser() {
-  const api = "https://jsonplaceholder.typicode.com/users";
-  const response = await fetch(api);
-  const data = await response.json();
-  const result = data
-    .filter((item) => item.name.toLowerCase().includes("k"))
-    .map(({ name, email }) => ({ name, email }));
-  console.log(result);
+  try {
+    const api = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(api);
+    const data = await response.json();
+    const result = data
+      .filter((item) => item.name.toLowerCase().includes("k"))
+      .map(({ name, email }) => ({ name, email }));
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 getUser();
