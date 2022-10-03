038ff9c9eabddad867c0276f6be1878b66e9b5b0
arafat-alim <arafat97alim@gmail.com>
2022-10-03 20:46:38 +0530

    pattern problem no 2 is solved

--- pattern/pattern-2.js
+++ pattern/pattern-2.js
@@ -1,1 +1,20 @@
-//! pattern Problem
+//! Pattern Problem
+//!
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+function pattern(num) {
+  let str = "";
+  for (let row = 1; row <= num; row++) {
+    for (let col = 1; col <= num; col++) {
+      str += "* ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+
+console.log(pattern(5));
